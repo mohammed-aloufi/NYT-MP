82a44c9c02df55cc982b53c9aeb1ffed547bb3a4
mohamed <maloufi@jahez.net>
2023-06-08 21:53:35 +0300

    created packages

--- app/src/main/java/com/example/nyt_mp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/nyt_mp/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.nyt_mp
+package com.example.nyt_mp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,14 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.nyt_mp.ui.theme.NYTMPTheme
+import com.example.nyt_mp.presentation.ui.theme.NYTMPTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NYTMPTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
--- app/src/main/java/com/example/nyt_mp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nyt_mp/presentation/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.nyt_mp.ui.theme
+package com.example.nyt_mp.presentation.ui.theme
 
 import android.app.Activity
 import android.os.Build
